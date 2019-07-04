f4407204870ecc7fbadfab344693be7cb793f0b9
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-07-04 20:28:14 +0200

    added Product component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,33 @@
 <script>
+    import Product from './Product.svelte';
+
 	export let name;
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+    section {
+        padding: 1rem;
+        width: 30rem;
+    }
+
+    label, input, textarea {
+        width: 100%;
+    }
 </style>
 
-<h1>Hello {name}!</h1>
+<section>
+    <div>
+        <label for="title">Title</label>
+        <input id="title" type="text">
+    </div>
+    <div>
+        <label for="price">Price</label>
+        <input id="price" type="number">
+    </div>
+    <div>
+        <label for="description">Description</label>
+        <textarea id="description" rows="3" />
+    </div>
+</section>
+
+<Product />
---
+++ src/Product.svelte
@@ -0,0 +1,39 @@
+<style>
+    div {
+        margin: 1rem;
+        box-shadow: 0 2px 8px rgba(0,0,0,0.25);
+        padding: 1rem;
+        border-radius: 5px;
+        background: white;
+    }
+
+     h1 {
+         font-size: 1.25rem;
+         margin: 0.25rem 0;
+     }
+
+     h2 {
+         font-size: 1rem;
+         margin: 0.25rem 0;
+         color: #4b4b4b;
+     }
+
+     p {
+         margin: 0.25rem 0;
+     }
+
+     button {
+         font-size: inherit;
+         padding: 0.15rem 0.5rem;
+         background: #d10057;
+         color: white;
+         cursor: pointer;
+     }
+</style>
+
+<div>
+    <h1>TITLE</h1>
+    <h2>PRICE</h2>
+    <p>DESCRIPTION</p>
+    <button>Add to cart</button>
+</div>
